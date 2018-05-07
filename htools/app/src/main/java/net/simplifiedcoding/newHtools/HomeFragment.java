13adc4203381249363c6605ef12f763d9787f712
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-05-07 12:38:52 -0300

    add graph

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
@@ -1,5 +1,6 @@
 package net.simplifiedcoding.newHtools;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import net.simplifiedcoding.bottomnavigationexample.R;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 /**
  * Created by Belal on 1/23/2018.
@@ -21,6 +25,24 @@ public class HomeFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_home, null);
+
+//        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+//
+//        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+//        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+//        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+//        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+//
+//        mPieChart.startAnimation();
+        View vhome = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        PieChart mPieChart = (PieChart) vhome.findViewById(R.id.piechart);
+        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+
+        return vhome;
     }
 }
