2a5596692a02ef8d845bd6a16913bc62d8ea5b0d
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-05-07 12:53:51 -0300

    add graph

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
@@ -11,7 +11,10 @@ import android.view.ViewGroup;
 import net.simplifiedcoding.bottomnavigationexample.R;
 
 import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.ValueLineChart;
 import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 /**
  * Created by Belal on 1/23/2018.
@@ -43,6 +46,26 @@ public class HomeFragment extends Fragment {
         mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
         mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
 
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) vhome.findViewById(R.id.cubiclinechart);
+        
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+        series.addPoint(new ValueLinePoint("Jan", 2.4f));
+        series.addPoint(new ValueLinePoint("Feb", 3.4f));
+        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
         return vhome;
     }
 }
