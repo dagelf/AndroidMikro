d6751935c1e1fe39829892e32e36dd6df86a2fbb
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-05-07 14:44:07 -0300

    modificando user

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
@@ -50,17 +50,17 @@ public class HomeFragment extends Fragment {
         ValueLineSeries series = new ValueLineSeries();
         series.setColor(0xFF56B7F1);
 
-        series.addPoint(new ValueLinePoint("Jan", 2.4f));
-        series.addPoint(new ValueLinePoint("Feb", 3.4f));
-        series.addPoint(new ValueLinePoint("Mar", .4f));
+        series.addPoint(new ValueLinePoint("Jan", 5));
+        series.addPoint(new ValueLinePoint("Feb", 6));
+        series.addPoint(new ValueLinePoint("Mar", 3));
         series.addPoint(new ValueLinePoint("Apr", 1.2f));
         series.addPoint(new ValueLinePoint("Mai", 2.6f));
-        series.addPoint(new ValueLinePoint("Jun", 1.0f));
+        series.addPoint(new ValueLinePoint("Jun", 1));
         series.addPoint(new ValueLinePoint("Jul", 3.5f));
         series.addPoint(new ValueLinePoint("Aug", 2.4f));
-        series.addPoint(new ValueLinePoint("Sep", 2.4f));
+        series.addPoint(new ValueLinePoint("Sep", 5));
         series.addPoint(new ValueLinePoint("Oct", 3.4f));
-        series.addPoint(new ValueLinePoint("Nov", .4f));
+        series.addPoint(new ValueLinePoint("Nov", 4));
         series.addPoint(new ValueLinePoint("Dec", 1.3f));
 
         mCubicValueLineChart.addSeries(series);
