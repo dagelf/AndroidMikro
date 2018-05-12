0535a18ef75ab79db0a3ca9f151f5b0890504e2f
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-05-12 11:24:51 -0300

    function salvar

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/users.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/users.java
@@ -3,6 +3,7 @@ package net.simplifiedcoding.newHtools;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,8 +14,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 import net.simplifiedcoding.bottomnavigationexample.R;
 
 
@@ -58,11 +71,32 @@ public class users extends Fragment {
         final String usuario = editEmail.getText().toString().trim();
         String senha = editSenha.getText().toString().trim();
         String nome = editNome.getText().toString().trim();
+
         String data = editData.getText().toString().trim();
 
-
-        Toast toast = Toast.makeText(getView().getContext(), "Clickko - "+nome,Toast.LENGTH_LONG);
-        toast.show();
+        if(usuario.equals("")){
+            editEmail.setError("Preencha este Campo !");
+            editEmail.requestFocus();
+            return;
+        }
+        if(senha.equals("")){
+            editSenha.setError("Preencha este Campo !");
+            editSenha.requestFocus();
+            return;
+        }
+        if(nome.equals("")){
+            editSenha.setError("Preencha este Campo !");
+            editSenha.requestFocus();
+            return;
+        }
+//
+//        mAuth.createUserWithEmailAndPassword(usuario,senha)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//
+//                });
+        //
+//        Toast toast = Toast.makeText(getView().getContext(), "Clickko - "+nome,Toast.LENGTH_LONG);
+//        toast.show();
     }
 
 
