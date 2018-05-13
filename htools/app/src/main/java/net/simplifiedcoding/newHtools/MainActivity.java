3d0debb2dcf2e86f5689f8bb66a62791c32ae2f5
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-13 16:49:48 -0300

    add api

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/MainActivity.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,11 +31,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("nomes");
-        myRef.setValue("Hello World");
+       // myRef.setValue("Hello World");
 
 
         //loading the default fragment
         loadFragment(new HomeFragment());
+
+        Fragment f = new users();
 
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
