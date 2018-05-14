6f87301fe91ea521e296c4fac665f7445b9eb519
Jeferson Araujo <jefersonaraujo95@gmail.com>
2018-05-14 14:16:04 -0300

    config

---
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/Config.java
@@ -0,0 +1,11 @@
+package net.simplifiedcoding.newHtools;
+
+
+
+public class Config {
+
+    public static final String HOST = "172.16.60.1";
+    public static final String USERNAME = "admin";
+    public static final String PASSWORD = "";
+
+}
