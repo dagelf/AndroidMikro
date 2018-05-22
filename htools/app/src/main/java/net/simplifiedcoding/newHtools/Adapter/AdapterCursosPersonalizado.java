b3c24ec7b6123207048271a6e5ddee95b97e339f
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-22 08:01:01 -0300

    view

---
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/Adapter/AdapterCursosPersonalizado.java
@@ -0,0 +1,67 @@
+package net.simplifiedcoding.newHtools.Adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.simplifiedcoding.bottomnavigationexample.R;
+import net.simplifiedcoding.newHtools.Pessoa;
+
+import java.util.List;
+
+public class AdapterCursosPersonalizado extends BaseAdapter {
+
+    private final List<Pessoa> pessoas;
+    private final Activity act;
+
+
+    public AdapterCursosPersonalizado(List<Pessoa> pessoas, Activity act) {
+        this.pessoas = pessoas;
+        this.act = act;
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return pessoas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return pessoas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = act.getLayoutInflater()
+                .inflate(R.layout.list_item, parent, false);
+
+
+        Pessoa pessoa = pessoas.get(position);
+
+        TextView nome = (TextView)
+                view.findViewById(R.id.lista_curso_personalizada_nome);
+        TextView descricao = (TextView)
+                view.findViewById(R.id.lista_curso_personalizada_descricao);
+        ImageView imagem = (ImageView)
+                view.findViewById(R.id.lista_curso_personalizada_imagem);
+
+        nome.setText(pessoa.getNome());
+        descricao.setText(pessoa.getEmail());
+        imagem.setImageResource(R.drawable.ic_people);
+
+        return view;
+    }
+
+
+}
---
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/Pessoa.java
@@ -0,0 +1,36 @@
+package net.simplifiedcoding.newHtools;
+
+/**
+ * Created by jeferson on 22/05/18.
+ */
+
+public class Pessoa {
+
+    private String nome;
+    private String email;
+    private String uid;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
