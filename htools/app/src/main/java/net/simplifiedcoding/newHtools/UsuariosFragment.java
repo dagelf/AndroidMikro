09976f8e6f5e862b6193f41b8b23a1f194cb8a84
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-23 15:54:19 -0300

    new class

---
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/UsuariosFragment.java
@@ -0,0 +1,17 @@
+package net.simplifiedcoding.newHtools;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jeferson on 23/05/18.
+ */
+
+public class UsuariosFragment extends Fragment {
+    ArrayList<Pessoa> pessoas= new ArrayList<>();
+    CustomAdapter customAdapter;
+    private Activity activity;
+
+}
