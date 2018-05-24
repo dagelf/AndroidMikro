6122689902fc29fb1fede611d2679f05d83192a7
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-24 13:05:39 -0300

    layout contrai

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/DashboardFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/DashboardFragment.java
@@ -39,24 +39,24 @@ public class DashboardFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View vDash = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final ListView listView = (ListView) vDash.findViewById(R.id.listView);
+        View vDash = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        //final ListView listView = (ListView) vDash.findViewById(R.id.listView);
 
-        final FloatingActionButton bntPlus = (FloatingActionButton) vDash.findViewById(R.id.btNovo);
-        bntPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getView().getContext(), "Click evento ",Toast.LENGTH_LONG);
-                toast.show();
-                Intent it = new Intent(getActivity(), ScrollingActivity.class);
-                startActivity(it);
+//        final FloatingActionButton bntPlus = (FloatingActionButton) vDash.findViewById(R.id.btNovo);
+//        bntPlus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast toast = Toast.makeText(getView().getContext(), "Click evento ",Toast.LENGTH_LONG);
+//                toast.show();
+//                Intent it = new Intent(getActivity(), ScrollingActivity.class);
+//                startActivity(it);
 //                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 //                transaction.replace(R.id.activity_montar_lista_inferior, new FavoritosFragment());
 //                transaction.commit();
 
-
-            }
-        });
+//
+//            }
+//        });
 //        View convertview;
 //        ViewGroup viewGroup;
 //
@@ -68,17 +68,17 @@ public class DashboardFragment extends Fragment {
 //
 //        }
 
-        Pessoa p = new Pessoa();
-        Pessoa p1 = new Pessoa();
-        p.setEmail("joao@gmail.com");
-        p.setNome("Joao Araujo de Sousa");
-        p1.setEmail("rosa@gmail.com");
-        p1.setNome("Rosa Araujo de Sousa");
-        pessoas.add(p);
-        pessoas.add(p1);
-
-        dashAdapter = new DashAdapter(getActivity(), pessoas);
-        listView.setAdapter((ListAdapter) dashAdapter);
+//        Pessoa p = new Pessoa();
+//        Pessoa p1 = new Pessoa();
+//        p.setEmail("joao@gmail.com");
+//        p.setNome("Joao Araujo de Sousa");
+//        p1.setEmail("rosa@gmail.com");
+//        p1.setNome("Rosa Araujo de Sousa");
+//        pessoas.add(p);
+//        pessoas.add(p1);
+//
+//        dashAdapter = new DashAdapter(getActivity(), pessoas);
+//        listView.setAdapter((ListAdapter) dashAdapter);
 //        FirebaseClient f = new FirebaseClient(getActivity(),listView);
 //        f.getUsers();
 //        Integer c =  f.usersCount();
