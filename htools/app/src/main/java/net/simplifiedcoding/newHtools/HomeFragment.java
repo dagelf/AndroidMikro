e14c4022a28a86b36a2bc1cd855a175e8f5a95ac
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-24 08:47:41 -0300

    grafico card

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
@@ -10,8 +10,10 @@ import android.view.ViewGroup;
 
 import net.simplifiedcoding.bottomnavigationexample.R;
 
+import org.eazegraph.lib.charts.BarChart;
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
 import org.eazegraph.lib.models.PieModel;
 import org.eazegraph.lib.models.ValueLinePoint;
 import org.eazegraph.lib.models.ValueLineSeries;
@@ -37,7 +39,21 @@ public class HomeFragment extends Fragment {
 //        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
 //
 //        mPieChart.startAnimation();
-        View vhome = inflater.inflate(R.layout.fragment_home, container, false);
+     View vhome = inflater.inflate(R.layout.fragment_home, container, false);
+//        BarChart mBarChart = (BarChart) vhome.findViewById(R.id.barchart);
+//
+//        mBarChart.addBar(new BarModel(50, 0xFF123456));
+//        mBarChart.addBar(new BarModel(36,  0xFF343456));
+
+//        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+//        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+//        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+//        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+//        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+//
+//        mBarChart.startAnimation();
+
 
 
         PieChart mPieChart = (PieChart) vhome.findViewById(R.id.piechart);
@@ -45,26 +61,26 @@ public class HomeFragment extends Fragment {
         mPieChart.addPieSlice(new PieModel("Femino", 45, Color.parseColor("#56B7F1")));
         mPieChart.addPieSlice(new PieModel("Outros", 5, Color.parseColor("#CDA67F")));
 
-        ValueLineChart mCubicValueLineChart = (ValueLineChart) vhome.findViewById(R.id.cubiclinechart);
-
-        ValueLineSeries series = new ValueLineSeries();
-        series.setColor(0xFF56B7F1);
-
-        series.addPoint(new ValueLinePoint("Jan", 5));
-        series.addPoint(new ValueLinePoint("Feb", 6));
-        series.addPoint(new ValueLinePoint("Mar", 3));
-        series.addPoint(new ValueLinePoint("Apr", 1.2f));
-        series.addPoint(new ValueLinePoint("Mai", 2.6f));
-        series.addPoint(new ValueLinePoint("Jun", 1));
-        series.addPoint(new ValueLinePoint("Jul", 3.5f));
-        series.addPoint(new ValueLinePoint("Aug", 2.4f));
-        series.addPoint(new ValueLinePoint("Sep", 5));
-        series.addPoint(new ValueLinePoint("Oct", 3.4f));
-        series.addPoint(new ValueLinePoint("Nov", 4));
-        series.addPoint(new ValueLinePoint("Dec", 1.3f));
-
-        mCubicValueLineChart.addSeries(series);
-        mCubicValueLineChart.startAnimation();
+//        ValueLineChart mCubicValueLineChart = (ValueLineChart) vhome.findViewById(R.id.cubiclinechart);
+//
+//        ValueLineSeries series = new ValueLineSeries();
+//        series.setColor(0xFF56B7F1);
+//
+//        series.addPoint(new ValueLinePoint("Jan", 5));
+//        series.addPoint(new ValueLinePoint("Feb", 6));
+//        series.addPoint(new ValueLinePoint("Mar", 3));
+//        series.addPoint(new ValueLinePoint("Apr", 1.2f));
+//        series.addPoint(new ValueLinePoint("Mai", 2.6f));
+//        series.addPoint(new ValueLinePoint("Jun", 1));
+//        series.addPoint(new ValueLinePoint("Jul", 3.5f));
+//        series.addPoint(new ValueLinePoint("Aug", 2.4f));
+//        series.addPoint(new ValueLinePoint("Sep", 5));
+//        series.addPoint(new ValueLinePoint("Oct", 3.4f));
+//        series.addPoint(new ValueLinePoint("Nov", 4));
+//        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+//
+//        mCubicValueLineChart.addSeries(series);
+//        mCubicValueLineChart.startAnimation();
         return vhome;
     }
 }
