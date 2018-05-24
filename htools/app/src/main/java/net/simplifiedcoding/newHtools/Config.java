bd3556da4519e1526d8bb6db563a3d07a6d8e26b
jefersonaraujo <jefersonaraujo95@gmail.com>
2018-05-24 18:30:50 -0300

    update

--- htools/app/src/main/java/net/simplifiedcoding/newHtools/Config.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/Config.java
@@ -4,7 +4,7 @@ package net.simplifiedcoding.newHtools;
 
 public class Config {
 
-    public static final String HOST = "172.16.60.1";
+    public static final String HOST = "192.168.0.112";
     public static final String USERNAME = "hotspot";
     public static final String PASSWORD = "hotspot";
 
--- htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
+++ htools/app/src/main/java/net/simplifiedcoding/newHtools/HomeFragment.java
@@ -40,19 +40,19 @@ public class HomeFragment extends Fragment {
 //
 //        mPieChart.startAnimation();
  View vhome = inflater.inflate(R.layout.fragment_home, container, false);
-        BarChart mBarChart = (BarChart) vhome.findViewById(R.id.barchart);
-
-        mBarChart.addBar(new BarModel(50, 0xFF123456));
-        mBarChart.addBar(new BarModel(36,  0xFF343456));
-
-        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
-        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
-        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
-        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-
-        mBarChart.startAnimation();
+//        BarChart mBarChart = (BarChart) vhome.findViewById(R.id.barchart);
+//
+//        mBarChart.addBar(new BarModel(50, 0xFF123456));
+//        mBarChart.addBar(new BarModel(36,  0xFF343456));
+//
+//        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+//        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+//        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+//        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+//        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+//        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+//
+//        mBarChart.startAnimation();
 
 
 
@@ -61,26 +61,24 @@ public class HomeFragment extends Fragment {
         mPieChart.addPieSlice(new PieModel("Femino", 45, Color.parseColor("#FE6DA8")));
 
 
-//        ValueLineChart mCubicValueLineChart = (ValueLineChart) vhome.findViewById(R.id.cubiclinechart);
-//
-//        ValueLineSeries series = new ValueLineSeries();
-//        series.setColor(0xFF56B7F1);
-//
-//        series.addPoint(new ValueLinePoint("Jan", 5));
-//        series.addPoint(new ValueLinePoint("Feb", 6));
-//        series.addPoint(new ValueLinePoint("Mar", 3));
-//        series.addPoint(new ValueLinePoint("Apr", 1.2f));
-//        series.addPoint(new ValueLinePoint("Mai", 2.6f));
-//        series.addPoint(new ValueLinePoint("Jun", 1));
-//        series.addPoint(new ValueLinePoint("Jul", 3.5f));
-//        series.addPoint(new ValueLinePoint("Aug", 2.4f));
-//        series.addPoint(new ValueLinePoint("Sep", 5));
-//        series.addPoint(new ValueLinePoint("Oct", 3.4f));
-//        series.addPoint(new ValueLinePoint("Nov", 4));
-//        series.addPoint(new ValueLinePoint("Dec", 1.3f));
-//
-//        mCubicValueLineChart.addSeries(series);
-//        mCubicValueLineChart.startAnimation();
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) vhome.findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+
+
+        series.addPoint(new ValueLinePoint("Apr", 5));
+        series.addPoint(new ValueLinePoint("Mai", 16));
+        series.addPoint(new ValueLinePoint("Jun", 10));
+        series.addPoint(new ValueLinePoint("Jul", 13));
+        series.addPoint(new ValueLinePoint("Aug", 4));
+        series.addPoint(new ValueLinePoint("Sep", 5));
+        series.addPoint(new ValueLinePoint("Oct", 10));
+        series.addPoint(new ValueLinePoint("Nov", 4));
+        series.addPoint(new ValueLinePoint("Dec", 8));
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
         return vhome;
     }
 }
